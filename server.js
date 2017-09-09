c842a24701412f44194e527602a48aad99c6f351
vijay <vjmoodle@gmail.com>
2017-09-08 21:34:53 -0400

    version2

--- server.js
+++ server.js
@@ -8,8 +8,13 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+ //
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
@@ -70,7 +75,7 @@ app.get('/', function (req, res) {
       res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
     });
   } else {
-    res.render('index.html', { pageCountMessage : null});
+    res.render('pages/index', { pageCountMessage : null});
   }
 });
 
@@ -88,7 +93,14 @@ app.get('/pagecount', function (req, res) {
     res.send('{ pageCount: -1 }');
   }
 });
-
+//
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+//
+app.get('/contact', function(req, res) {
+    res.render('pages/contact');
+});
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
