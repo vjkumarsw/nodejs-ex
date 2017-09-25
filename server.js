b46ad7dbf6ca34076c1689f14489310d7d7d3963
vijay <vjmoodle@gmail.com>
2017-09-24 21:47:22 -0400

    version2

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
  //
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
